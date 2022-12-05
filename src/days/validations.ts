8b251ba7ea3f33db627127241e605884a6bc69fe
jtgi <j@jtgi.me>
2022-12-05 20:44:57 +0900

    asdf

--- src/days/validations.ts
+++ src/days/validations.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const UpdateDay = z.object({
   id: z.number(),
-  date: z.date().optional(),
   roaster: z.string(),
   location: z.string(),
   coffee: z.string(),
@@ -15,7 +14,6 @@ export const UpdateDay = z.object({
 })
 
 export const CreateDay = z.object({
-  date: z.date().optional(),
   roaster: z.string(),
   location: z.string(),
   coffee: z.string(),
